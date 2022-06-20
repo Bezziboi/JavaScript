5b14b9afc4cc1c0d22fe37087ab3989ffc01b8e3
Bezziboi <bezirgen.09142000@mail.ru>
2022-06-20 19:01:43 +0300

    added JS_HW_1.2.js

--- JS_HW_1.2.js
+++ JS_HW_1.2.js
@@ -75,18 +75,19 @@ was skipped when converted to a number
 
     const checkAge3 = function(age){
         age = Number(age)
-    if (!isNaN(age)) {
-    console.log("Invalid input")
-    }
-    else if(age < 18) { 
-        console.log("You don't have access 'cause your age is " + age + " It's less than " + 18) 
-    }
-    else if (age >= 18 && age < 61) {
-        console.log("Welcome!")
-    }
-    else if (age > 60) {
-        console.log("Keep calm and look Culture channel")
-    }
+    if (age && !isNaN(age)) {
+        if (age < 18){  
+            console.log("You don't have access 'cause your age is " + age + " It's less than " + 18) 
+        }
+        else if (age >= 18 && age < 61) {
+            console.log("Welcome!")
+        }
+        else  (age > 60); {
+            console.log("Keep calm and look Culture channel")
+        }
+        }
+        else  console.log("Invalid input")
+        
     }
     checkAge3('qwe')
     checkAge3('')
