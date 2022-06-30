da8dccf4ac8782789acf7416b5df7c80212b9c94
Bezziboi <bezirgen.09142000@mail.ru>
2022-06-30 23:36:54 +0300

    folder js_hw_3

---
+++ javaScript_HW_3/JS_HW_3_4.js
@@ -0,0 +1,6 @@
+/*  Task 4****
+write a function:
+The function builds a tree-like list of the company.
+When it is called, a list of company departments should be displayed in the console,
+indicating the number of employees and respecting the nesting of departments.
+*/
