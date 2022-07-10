2143920fbc4d1ec494bf31bebcb16293ff7cd9f1
Bezziboi <bezirgen.09142000@mail.ru>
2022-07-11 00:21:33 +0300

    extra js file

--- extra.js
+++ extra.js
@@ -1100,3 +1100,43 @@
 
 
 
+
+
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Rabbit extends Animal {
+    
+    constructor(name, age, runSpeed) {
+        super(name, age);
+        this.runSpeed = runSpeed;
+    }
+}
+
+class Fish extends Animal {
+    
+    constructor(name, age, swimSpeed) {
+        super(name, age);
+        this.swimSpeed = swimSpeed;
+    }
+}
+
+class Hawk extends Animal {
+    
+    constructor(name, age,flySpeed) {
+        super(name, age);
+        this.flySpeed = flySpeed;
+    }
+}
+
+const rabbit = new Rabbit("rabbit", 1, 20);
+const fish = new Fish("fish", 2, 30);
+const hawk = new Hawk("hawk", 3, 50);
+
+console.log(rabbit.name);
+console.log(rabbit.age);
+console.log(rabbit.runSpeed);
