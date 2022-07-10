8f79ba2e5e6dc81b8d66476b95d400e9d3e28011
Bezziboi <bezirgen.09142000@mail.ru>
2022-07-11 00:05:03 +0300

    extra js file

--- extra.js
+++ extra.js
@@ -1052,43 +1052,51 @@
 
 
 
-class Animal {
-    alive = true;
+// class Animal {
+//     alive = true;
 
-    eat() {
-        console.log(`This ${this.name} is eating`);
-    }
-    sleep() {
-        console.log(`This ${this.name} is sleeping`);
-    }
-}
-class Rabbit extends Animal {
-    name = "rabbit"
+//     eat() {
+//         console.log(`This ${this.name} is eating`);
+//     }
+//     sleep() {
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+// }
+// class Rabbit extends Animal {
+//     name = "rabbit"
 
-    run() {
-        console.log(`This ${this.name} is running`);
-    }
-}
-class Fish extends Animal {
-    name = "fish"
+//     run() {
+//         console.log(`This ${this.name} is running`);
+//     }
+// }
+// class Fish extends Animal {
+//     name = "fish"
 
-    swim() {
-        console.log(`This ${this.name} is swimming`);
-    }
-}
-class Hawk extends Animal {
-    name = "hawk"
+//     swim() {
+//         console.log(`This ${this.name} is swimming`);
+//     }
+// }
+// class Hawk extends Animal {
+//     name = "hawk"
 
-    fly() {
-        console.log(`This ${this.name} is flying`);
-    }
-}
+//     fly() {
+//         console.log(`This ${this.name} is flying`);
+//     }
+// }
 
-const rabbit = new Rabbit();
-const fish = new Fish();
-const hawk = new Hawk();
+// const rabbit = new Rabbit();
+// const fish = new Fish();
+// const hawk = new Hawk();
 
-console.log(rabbit.alive);
-rabbit.eat();
-rabbit.sleep();
-rabbit.run();
+// console.log(rabbit.alive);
+// rabbit.eat();
+// rabbit.sleep();
+// rabbit.run();
+
+
+
+
+
+
+
+
