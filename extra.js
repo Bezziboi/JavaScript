6ecdf88811165614895ef48dadb8263f4577b32a
Bezziboi <bezirgen.09142000@mail.ru>
2022-07-11 15:46:23 +0300

    extra js file

--- extra.js
+++ extra.js
@@ -1268,3 +1268,33 @@
 
 
 
+// class Card {
+//     constructor(value, suit) {
+//         this.value = value;
+//         this.suit = suit;
+//     }
+// }
+
+// let cards = [new Card("A", "Hearts"),
+//              new Card("A", "Spands"),
+//              new Card("A", "Diamonds"),
+//              new Card("A", "Clubs"),
+//              new Card("2", "Hearts"),
+//              new Card("2", "Spands"),
+//              new Card("2", "Diamonds"),
+//              new Card("2", "Clubs")];
+
+// cards.forEach(card => console.log(card.value, card.suit));
+
+
+
+
+
+
+
+
+
+
+
+
+
