8ca1db16d8d3d0816ed13cea4d6acf0c2c7c34e3
Bezziboi <bezirgen.09142000@mail.ru>
2022-07-17 22:29:53 +0300

    extra js

--- extra.js
+++ extra.js
@@ -1827,3 +1827,72 @@
 //     })
 //     return result;
 // }
+
+
+
+
+
+
+
+
+const myLabel = document.querySelector("#myLabel");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+let milsecs = 0;
+let paused = true;
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let intervalId;
+
+startBtn.addEventListener("click", () => {
+    if(paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(uploadTime, 1);
+    }
+})
+pauseBtn.addEventListener("click", () => {
+    if(!paused) {
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+})
+resetBtn.addEventListener("click", () => {
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    milsecs = 0;
+    paused = true;
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    intervalId;
+    clearInterval(intervalId);
+    myLabel.textContent = "00:00:00:00"
+})
+function uploadTime() {
+    elapsedTime = Date.now() - startTime;
+
+    milsecs = Math.floor((elapsedTime / 1000) * 1000 % 1000);
+    secs = Math.floor((elapsedTime / 1000) % 60);
+    mins = Math.floor((elapsedTime / (1000 * 60))% 60);
+    hrs = Math.floor((elapsedTime / (1000 * 60 * 60))% 60);
+
+    milsecs = pad(milsecs);
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad(hrs);
+
+    myLabel.textContent = `${hrs}:${mins}:${secs}:${milsecs}`;
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
